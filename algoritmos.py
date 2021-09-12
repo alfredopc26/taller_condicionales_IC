f5a4d23237f7e99419fad9e50edecc711bb1c414
alfredopc26 <calfredoperez26@gmail.com>
2021-09-12 11:34:43 -0500

    Punto 2 descuento numero al azar

--- algoritmos.py
+++ algoritmos.py
@@ -4,6 +4,9 @@ Created on Sun Sep 12 11:06:49 2021
 
 @author: Carlos Perez
 """
+import random
+
+
 #
 #Punto 1
 #Calcular total a pagar por camisas con descuento por mas de 3 camisas
@@ -25,3 +28,23 @@ def total_pago(cantidad, precio):
 precio = int(input('Digite el precio de la camisa: \n'))
 cantidad = int(input('Digite la cantidad a comprar: \n'))
 print(f'El valor total a pagar es : {total_pago(cantidad, precio)}')
+#
+#Punto 2
+#Descuento por numero escogido al azar
+#
+
+def descuento_num_azar(subtotal):
+    
+    
+    num = random.randint(0, 99)
+    if num >= 74:
+        descuento = subtotal * 0.20
+    else:
+        descuento = subtotal * 0.15
+    
+    return { 'num': num, 'descuento': descuento }
+
+subtotal = int(input('Digite el subtotal de la compra: \n'))
+result = descuento_num_azar(subtotal)
+print(f"El numero al azar es : {result['num']}")
+print(f"El descuento es de : $ {result['descuento']}")
